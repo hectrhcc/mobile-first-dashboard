70e20845faa97f000258453551ce00da4676bf46
Hector <hcontrerascorvacho@gmail.com>
2024-03-22 23:05:39 -0300

    fin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
     }, [darkMode])
 
   return (
-    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235] rounded-b-[20px] pt-8 px-6 mb-[1000px] md:mb-[560px] lg:mb-[320px]  xl:mb-[290px] sm:mb-[530px]">
+    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235] rounded-b-[20px] pt-8 px-6 mb-[1000px] md:mb-[560px] lg:mb-[320px]  xl:mb-[340px] sm:mb-[530px]">
     <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-0">Social Media Dashboard</h1>
     <div className="md:flex md:justify-between items-center ">
     <p className="text-Dark-Grayish-Blue  dark:text-Desaturated-Blue font-bold mb-0">Total Followers: 23,004</p>
